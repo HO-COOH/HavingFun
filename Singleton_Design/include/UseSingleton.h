d2ed5d1aa852fb93703ca0d6963cab44a4efaf53
HO-COOH <szpeter@yeah.net>
2020-02-06 05:47:23 -0600

    Move Singleton_Design to ProgrammingNote/DesignPattern

--- Singleton_Design/include/UseSingleton.h
+++
@@ -1,3 +0,0 @@
-#include "MySingleton.h"
-
-void UseSingleton();
--- Singleton_Design/src/UseSingleton.cpp
+++
@@ -1,9 +0,0 @@
-#include "../include/UseSingleton.h"
-#include <cstdio>
-
-void UseSingleton()
-{
-	puts("Use MySingleton in UseSingleton.cpp");
-	std::cout << "My singleton in UseSingleton.cpp : " << &singleton_obj << '\n';
-	singleton_obj.use();
-}
